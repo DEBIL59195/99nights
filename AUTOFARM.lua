b3a978f4e51728c031e7c7baccae0cc4a7b9d1e6
DEBIL59195 <prodavec4466@gmail.com>
2025-08-23 16:49:55 -0700

    Create AUTOFARM.lua

---
+++ AUTOFARM.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Rx1m/CpsHub/refs/heads/main/99%20days%20at%20the%20forest%20Diamond%20Farm"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/DEBIL59195/99nights/refs/heads/main/pro.lua"))()
